f412db440e153bebd7c5378f06ab83436c8930f3
AlexeyTolmachov <taar12sh@gmail.com>
2023-10-06 16:59:37 +0300

    fised importData

--- src/importData.js
+++ src/importData.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 const db = require('./mysqlDB/database');
+const path = require('path');
 
 async function importDataFromCSV(filePath) {
 	try {
 		fs.createReadStream(filePath)
 			.pipe(csv())
 			.on('data', async (row) => {
-
 				const existingRecord = await db.promise().query('SELECT * FROM testimonials WHERE unique_employee_number = ?', [row.Unique_employee_number]);
 				if (existingRecord[0].length === 0) {
 					await db.promise().query('INSERT INTO testimonials (Reviewer, Email, Review, Rating, Employee, employee_position, Unique_employee_number, Company, company_description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [
@@ -28,11 +28,38 @@ async function importDataFromCSV(filePath) {
 			})
 			.on('end', () => {
 				console.log('Import complete.');
+
+				fs.unlink(filePath, (err) => {
+					if (err) {
+						console.error('An error occurred while deleting the file:', err);
+					} else {
+						console.log('File deleted successfully.');
+					}
+				});
+
 			});
 	} catch (error) {
 		console.error('An error occurred while importing data:', error);
 	}
 }
 
-const csvFilePath = '../reviews.csv';
-importDataFromCSV(csvFilePath);
+const importDataFromCSVController = async (req, res) => {
+	try {
+		const directoryPath = path.resolve(__dirname, '../uploads/');
+		const files = fs.readdirSync(directoryPath);
+		const csvFiles = files.filter((file) => file.endsWith('.csv'));
+		if (csvFiles.length === 0) {
+			return res.status(400).json({ error: 'No CSV files found in the directory' });
+		}
+		const filePath = path.join(directoryPath, csvFiles[0]);
+		await importDataFromCSV(filePath);
+		res.status(200).json({ message: 'Data imported successfully' });
+	} catch (error) {
+		console.error('An error occurred while importing data:', error);
+		res.status(500).json({ error: 'Internal server error' });
+	}
+};
+
+module.exports = importDataFromCSVController;
+
+
--- src/routes/testimonialsRoutes.js
+++ src/routes/testimonialsRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const multer = require("multer");
 const testimonialController = require('../controllers/testimonialController');
+const importDataFromCSVController = require('../importData');
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "uploads/");
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	}
+});
+const upload = multer({ storage: storage });
 router.get('/', testimonialController.getAllTestimonials);
 router.delete('/', testimonialController.deleteAllTestimonials);
-
-
+router.post('/', upload.single('csvData'), importDataFromCSVController);
 module.exports = router;
